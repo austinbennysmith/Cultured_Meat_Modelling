1c03c7af9d7bfa8d867232c28b1599dfbae5cd74
Benny Smith <asmit101@node1326.oscar.ccv.brown.edu>
2021-04-04 23:13:51 -0400

    added error analysis for x velocity

---
+++ learning_basilisk/couette_example/validation/L2Loop.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+level=[]
+errors = []
+for i in range(2, 8):
+	level.append(i)
+	filename = "xprof%d.txt" %i
+	xprof = np.genfromtxt(filename, delimiter=" ")
+	#print(xprof)
+	steady = xprof[-100:, 1]
+	#print("computed:", len(steady))
+	#print("computed:", steady)
+	theoretical = np.linspace(0, 1, 101)
+	theoretical = theoretical[:-1]
+	#print(theoretical)
+	#print("theoretical:", theoretical)
+	#print("theoretical:", len(theoretical))
+	error = np.linalg.norm(steady-theoretical)
+	#print("error:", error)
+	#other = np.linspace(-0.5, 0.5, 101)
+	#print(other)
+	errors.append(error)
+print(errors)
+plt.semilogy(level,errors)
+plt.title("Error vs Mesh Refinement")
+plt.xlabel("Level")
+plt.ylabel("L2 Norm")
+plt.show()
